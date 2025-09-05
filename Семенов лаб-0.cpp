5c953a1e2e409f2b8d3f4e405834b66fabfea25d
SemenovAA2410 <fvazochka@mail.ru>
2025-09-05 14:51:18 +0300

    Update Семенов лаб-0.cpp

--- Семенов лаб-0.cpp
+++ Семенов лаб-0.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-using namespace std;
+
 
 int main() {
-    cout << "Hello world" << endl;
-    cout << "Press any key to exit...";
-    cin.get();
-    return 0;
+    std::cout << "Hello world\n";
+
 }
+
